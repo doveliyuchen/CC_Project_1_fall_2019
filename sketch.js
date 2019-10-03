b50a96b419520cdc843c121f03ad5447236ce2dd
Dove Li <54865789+doveliyuchen@users.noreply.github.com>
2019-10-02 23:12:01 -0400

    first update of my project
    cute !!!!!!!!!! emoticon from japan to show the emotions
    :3 for example
    but more complex

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 function setup() {
-  // put setup code here
-  createCanvas(400, 400);
-  background(0);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  // put drawing code here
-  rect(300, 200, 10, 10);
+  background(220);
+  noFill();
+  arc(200,200,100,180,PI/2+0.5,PI+0.9)
+  arc(400,200,100,180,3*PI/2+0.7,PI/5+0.45)
+  
+  
 }
