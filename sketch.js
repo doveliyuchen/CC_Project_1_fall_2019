9325daf8275afd0711a66db91dd766861642bc5c
Dove Li <54865789+doveliyuchen@users.noreply.github.com>
2019-10-04 02:30:43 -0400

    update!!! will come out some cute things
    Hopefully, this will not think as some text....
    
    cuz I manually drew it.

--- sketch.js
+++ sketch.js
@@ -5,8 +5,31 @@ function setup() {
 function draw() {
   background(220);
   noFill();
-  arc(200,200,100,180,PI/2+0.5,PI+0.9)
-  arc(400,200,100,180,3*PI/2+0.7,PI/5+0.45)
-  
-  
+  push();
+  translate(60,150);
+  kuohao();
+  pop();
+  omiga();
+  eye();
 }
+
+
+function kuohao() {
+  arc(200,200,100,180,2.3,4.1)
+  translate(400,400);
+  rotate(PI);
+  arc(0,200,100,180,2.19,3.98)
+}
+
+function omiga() {
+  strokeWeight(4);
+  arc(350,350,20,30,0,3*PI/2)
+  arc(370,350,20,30,3*PI/2,PI)
+}
+function eye() {
+  strokeWeight(4);
+  ellipse(330,345,5,5);
+  ellipse(390,345,5,5);
+}
+// function ear() {
+//   triangle(310,345,
