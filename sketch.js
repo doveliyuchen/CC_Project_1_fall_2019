0ddcd9ec56a7e2683dd15614b8ba34b657a316b8
doveliyuchen <54865789+doveliyuchen@users.noreply.github.com>
2019-10-09 22:36:36 -0400

    Update sketch.js
    add another cat

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ function draw() {
   background(220);
   noFill();
    cat = new Cat(mouseX, mouseY, 10);
+   cute = new Cute();
+   cute.display();
   cat.display();
 }
 
@@ -87,6 +89,149 @@ class Map {
   }
 
 display() {
+
+}
+
+
+
+}
+
+class Cute{
+
+
+ display(){
+  background(220);
+  this.weiba();
+  this.body();
+  this.whiteBody();
+  this.bluePart();
+  this.ears();
+  this.head();
+
+}
+
+ weiba() {
+  strokeWeight(6);
+  beginShape();
+  //vertex(477, 438);
+  vertex(433, 458);
+  bezierVertex(492, 433, 496, 453, 484, 463);
+  vertex(433, 494);
+  fill("#ffffd9");
+  stroke("#33190c");
+  endShape();
+}
+
+ body() {
+  beginShape();
+  vertex(255, 392);
+  vertex(255, 526);
+  bezierVertex(256, 555, 282, 557, 293, 530);
+
+  vertex(383, 530);
+  bezierVertex(392, 557, 419, 558, 428, 526);
+  vertex(428, 526);
+  quadraticVertex(434, 523, 430, 393);
+  vertex(430, 393);
+  endShape();
+}
+
+ whiteBody() {
+  beginShape();
+  fill(255);
+  vertex(317, 501);
+  quadraticVertex(345, 504, 363, 500);
+  vertex(363, 500);
+  quadraticVertex(386, 492, 389, 480);
+  vertex(389, 480);
+  quadraticVertex(393, 469, 396, 437);
+  vertex(396, 437);
+
+  endShape();
+
+
+}
+
+ bluePart() {
+
+  fill("#e5f8ef");
+  beginShape();
+  vertex(428, 459);
+  bezierVertex(407, 456, 405, 479, 421, 482);
+  vertex(421, 482);
+  bezierVertex(393, 482, 394, 507, 427, 511);
+  vertex(427, 511);
+  quadraticVertex(429, 485, 428, 459);
+  vertex(428, 459);
+  endShape();
+}
+
+ ears() {
+  //        左边耳朵
+  stroke("#33190c");
+  fill("#ffffd9");
+  strokeWeight(4);
+  beginShape();
+  vertex(106, 79);
+  quadraticVertex(114, 4, 209, 14);
+  endShape();
+
+  //        右边耳朵
+  beginShape();
+  strokeWeight(5);
+
+  vertex(376, 14);
+  quadraticVertex(431, 13, 446, 15);
+  //vertex(446,15);
+  quadraticVertex(488, 20, 498, 48);
+  //vertex(498,48);
+  quadraticVertex(508, 75, 512, 110);
+
+  endShape();
+
+
+  beginShape();
+  strokeWeight(8);
+  vertex(470, 65);
+  vertex(483, 27);
+  endShape();
+
+  beginShape();
+  fill("#fecce5");
+  vertex(473, 65);
+  vertex(483, 30);
+
+  bezierVertex(502, 46, 496, 60, 512, 110);
+  //vertex(512, 110);
+  endShape();
+
+
+}
+//头
+ head() {
+  strokeWeight(10);
+  stroke("#33190c");
+  beginShape();
+  fill("#ffffd9")
+
+  vertex(280, 6);
+  bezierVertex(476, 0, 535, 154, 541, 166);
+  vertex(541, 166);
+  vertex(546, 180);
+  bezierVertex(584, 304, 530, 368, 432, 392);
+  endShape();
+  beginShape();
+  vertex(432, 392);
+  quadraticVertex(210, 460, 90, 372);
+  //vertex(90,372)
+  quadraticVertex(38, 324, 34, 314);
+  //vertex(34,314);
+  quadraticVertex(1, 268, 38, 174);
+  //vertex(38,174);
+  quadraticVertex(128, 8, 280, 6);
+
+  endShape();
+
 
 }
 
